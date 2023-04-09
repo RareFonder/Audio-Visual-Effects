b6eeaea715b97eafba4c72de43322815d444406e
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-09 15:42:18 +0800

    1 file modified

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 700;
 const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
   constructor(x, y) {
@@ -13,6 +14,18 @@ class Explosion {
     this.width = this.spriteWidth * 0.5;
     this.height = this.spriteHeight * 0.5;
     this.image = new Image();
-    this.image.src = ''
+    this.image.src = 'https://stackblitz.com/files/web-platform-vzkvag/github/RareFonder/Audio-Visual-Effects/main/boom.png';
+    this.frame = 0;
+  }
+  update() {
+    this.frame++;
+  }
+  draw() {
+    ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
   }
 }
+
+window.addEventListener('click', (e) => {
+  ctx.fillStyle = 'white'
+  ctx.fillRect(e.x - canvasPosition.left - 25, e.y - canvasPosition.top - 25, 50, 50);
+})
