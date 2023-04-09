2d8a87ab2223eed8df81e8df59446e22b1bd99a9
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-09 16:04:47 +0800

    1 file modified

--- script.js
+++ script.js
@@ -7,25 +7,51 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
     this.spriteWidth = 200;
     this.spriteHeight = 179;
     this.width = this.spriteWidth * 0.5;
     this.height = this.spriteHeight * 0.5;
+    this.x = x - this.width * 0.5;
+    this.y = y - this.height * 0.5;
     this.image = new Image();
     this.image.src = 'https://stackblitz.com/files/web-platform-vzkvag/github/RareFonder/Audio-Visual-Effects/main/boom.png';
     this.frame = 0;
+    this.timer = 0;
+    this.angle = Math.random() * 6.2;
   }
   update() {
-    this.frame++;
+    this.timer++;
+    if (this.timer % 4 === 0) this.frame++;
   }
   draw() {
-    ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0, 0, this.width, this.height);
+    ctx.restore();x
   }
-}
+};
 
-window.addEventListener('click', (e) => {
-  ctx.fillStyle = 'white'
-  ctx.fillRect(e.x - canvasPosition.left - 25, e.y - canvasPosition.top - 25, 50, 50);
-})
+window.addEventListener('mousemove', (e) => {
+  createAnimation(e);
+});
+
+const createAnimation = (e) => {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;;
+  explosions.push(new Explosion(positionX, positionY));
+};
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); 
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].update();
+    explosions[i].draw();
+    if (explosions[i].frame > 5) {
+      explosions.splice(i, 1); 
+      i--
+    }
+  }
+  requestAnimationFrame(animate);
+};
+animate();
